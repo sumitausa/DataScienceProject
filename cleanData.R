44eca89301f55fd5dff2163f2e00bb6fac61f81f
sumitausa <palanisumita@yahoo.com>
2018-04-15 10:09:05 -0400

    cleaning ppercentages

--- cleanData.R
+++ cleanData.R
@@ -11,6 +11,15 @@ fitness$NoStud7[fitness$NoStud7 == 0] <- NA
 
 fitness$NoStud9[fitness$NoStud9 == 0] <- NA
 
-View(fitness)
+#clean up the percentage columns
+ 
+fitness$Perc5b[is.na(fitness$NoStud5) & fitness$Perc5b ==0] <- NA    
+
+fitness$Perc7b[is.na(fitness$NoStud7) & fitness$Perc7b ==0] <- NA 
+
+fitness$Perc9b[is.na(fitness$NoStud9) & fitness$Perc9b ==0] <- NA 
+
+View(fitness)                
+                          
 
 write.csv(fitness,file = "C:/Users/s/Documents/DataScience/DataScienceProject/fitness_clean.csv")
