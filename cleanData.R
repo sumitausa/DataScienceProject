681a2fbb611c2ff1a45baee44f1481b27ae7bf06
sumitausa <palanisumita@yahoo.com>
2018-04-22 09:59:01 -0400

    Update cleanData.R
    adding ggplot for categorical columns

--- cleanData.R
+++ cleanData.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(tidyr)
+library(ggplot2)
 
 fitness <- read.csv(file="C:/Users/s/Documents/data.csv", header=TRUE, sep=",",stringsAsFactors=FALSE)
 
@@ -11,8 +12,17 @@ fitness$NoStud7[fitness$NoStud7 == 0] <- NA
 
 fitness$NoStud9[fitness$NoStud9 == 0] <- NA
 
+#clean up rows that have no data for number of students tested in grade 5, 7 and 9
+fitness <- fitness[!(is.na(fitness$NoStud5)) | !(is.na(fitness$NoStud7))| !(is.na(fitness$NoStud9)),]
+
+#Clean up rows that dont have data about the excercies
+fitness[!grepl("fitness", fitness$Line_Text),]
+
+
 #clean up the percentage columns
- 
+
+fitness$Perc5b[is.na(fitness$NoStud5) & fitness$Perc5a ==0] <- NA   
+
 fitness$Perc5b[is.na(fitness$NoStud5) & fitness$Perc5b ==0] <- NA    
 
 fitness$Perc7b[is.na(fitness$NoStud7) & fitness$Perc7b ==0] <- NA 
@@ -20,6 +30,16 @@ fitness$Perc7b[is.na(fitness$NoStud7) & fitness$Perc7b ==0] <- NA
 fitness$Perc9b[is.na(fitness$NoStud9) & fitness$Perc9b ==0] <- NA 
 
 View(fitness)                
-                          
+g <- ggplot(data = fitness) + geom_bar(mapping = aes(x = Report_Number))
+print(g)
 
-write.csv(fitness,file = "C:/Users/s/Documents/DataScience/DataScienceProject/fitness_clean.csv")
+g <- ggplot(data = fitness) + geom_bar(mapping = aes(x = factor(Line_Number)))
+print(g)
+g <- ggplot(data = fitness) + geom_bar(mapping = aes(x = factor(Line_Text)))
+
+print(g)
+g1 <- ggplot(data = fitness,mapping = aes(x= factor(Line_Number), y = SCHL)) + geom_line()
+print(g1)
+c <- fitness %>%   count(Line_Text)
+
+print (c)
